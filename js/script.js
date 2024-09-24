415b47754b4445252307aa51fb1798b6cd0ae8ab
MaleneLau <lale43321@gmail.com>
2024-09-24 14:29:22 +0200

    Små rettelse + muligt at se adgangskode
    Der er blevet ændret lidt forskellige steder, tilføjet lidt css og html. Derudover er der lavet så det er muligt at se og skjule sin adgangskode.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 document.getElementById('registrationForm').addEventListener('submit', function (e) {
     e.preventDefault(); 
 
-    //henter værdierne fra inputfelterne
     let name = document.getElementById('name').value.trim();
     let password = document.getElementById('password').value.trim();
     let confirmPassword = document.getElementById('confirmPassword').value.trim();
     let email = document.getElementById('email').value.trim();
     let telefon = document.getElementById('telefon').value.trim();
 
-    //henter Error meddelelseselementer
     let nameError = document.getElementById('nameError');
     let passwordError = document.getElementById('passwordError');
     let emailError = document.getElementById('emailError');
     let telefonError = document.getElementById('telefonError');
     let confirmPasswordError = document.getElementById('confirmPasswordError');
 
-    //initialisér gyldighed
     let isValid = true;
 
-    //Tjekker for navn
     if (name.length <2) {
         nameError.textContent = "Navn skal være mindst 2 karakterer. ";
         nameError.style.display = 'block'; 
@@ -27,7 +23,6 @@ document.getElementById('registrationForm').addEventListener('submit', function 
         nameError.style.display = 'none';
     }
 
-    // Tjek for adgangskode 
     const passwordPattern = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/; 
     if (!password.match(passwordPattern)) {
         passwordError.textContent = "Adgangskoden skal være mindst 8 karakterer, indeholde mindst 1 stort bogstav og 1 specialtegn !@#$%^&*."; 
@@ -67,3 +62,36 @@ document.getElementById('registrationForm').addEventListener('submit', function 
         alert("Formularen er korrekt udfyldt!"); 
     }
 })
+
+document.getElementById('iconPassword').addEventListener('click', function () {
+    let passwordField = document.getElementById('password');
+    let showIcon = document.getElementById('showIcon');
+    let hideIcon = document.getElementById('hideIcon');
+    
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text'; 
+        showIcon.style.display = 'none'; 
+        hideIcon.style.display = 'inline'; 
+    } else {
+        passwordField.type = 'password'; 
+        showIcon.style.display = 'inline'; 
+        hideIcon.style.display = 'none';
+    }
+});
+
+
+document.getElementById('iconConfirmPassword').addEventListener('click', function () { 
+    let confirmPasswordField = document.getElementById('confirmPassword');
+    let confirmShowIcon = document.getElementById('confirmShowIcon');
+    let confirmHideIcon = document.getElementById('confirmHideIcon');
+    
+    if(confirmPasswordField.type === 'password') {
+        confirmPasswordField.type = 'text';
+        confirmShowIcon.style.display = 'none';
+        confirmHideIcon.style.display = 'inline';
+    } else {
+        confirmPasswordField.type = 'password';
+        confirmShowIcon.style.display = 'inline';
+        confirmHideIcon.style.display = 'none';
+    }
+});
